5fbacdc6ac324559b53d2fe47dac488ba66ac65c
seminki <vivasemin@gmail.com>
2020-11-27 21:06:57 +0900

    qna @service,@depository 연결

--- Bombom/src/main/java/com/kh/bom/qna/model/service/QnaServiceImpl.java
+++ Bombom/src/main/java/com/kh/bom/qna/model/service/QnaServiceImpl.java
@@ -2,11 +2,12 @@ package com.kh.bom.qna.model.service;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kh.bom.qna.model.dao.QnaDao;
 
 
-
+@Service
 public class QnaServiceImpl implements QnaService {
 
 	@Autowired
