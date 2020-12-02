ca715e7521407fd610097512fe1f396812e2ed2b
sujeongkang <sujeong.dev@gmail.com>
2020-12-02 15:10:08 +0900

    #20-12-02 15:09 mvc어노테이션 처리 -sj-

--- Bombom/src/main/java/com/kh/bom/zzim/controller/ZzimController.java
+++ Bombom/src/main/java/com/kh/bom/zzim/controller/ZzimController.java
@@ -1,12 +1,20 @@
 package com.kh.bom.zzim.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.bom.zzim.model.service.ZzimService;
-
+@Controller
 public class ZzimController {
 	@Autowired
 	private ZzimService service;
 	
-	
+	@RequestMapping("/mypage/zzimList")
+	public ModelAndView selectZzimList(ModelAndView mv) {
+		
+		mv.setViewName("mypage/zzimList");
+		return mv;
+	}
 }
