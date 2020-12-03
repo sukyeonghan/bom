9189a5182d341eb4d2a2c42a15b521a8e1bbce5d
sujeongkang <sujeong.dev@gmail.com>
2020-12-03 23:58:50 +0900

    #20-12-03 23:58 찜폴더 추가 -sj-

--- Bombom/src/main/java/com/kh/bom/zzim/model/dao/ZzimDao.java
+++ Bombom/src/main/java/com/kh/bom/zzim/model/dao/ZzimDao.java
@@ -1,5 +1,15 @@
 package com.kh.bom.zzim.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kh.bom.zzim.model.vo.Zzim;
+
 public interface ZzimDao {
+
+	List<Zzim> selectZzimList(SqlSession session, String memNo);
+
+	int insertZzim(SqlSession session, Zzim z);
 
 }
