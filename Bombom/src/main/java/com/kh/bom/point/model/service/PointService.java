ef876d9582435695d5205daadd4b06b53818fed0
sujeongkang <sujeong.dev@gmail.com>
2021-01-07 07:40:51 +0900

    #마이페이지 적립금 내역 기간 검색 추가, ajax페이징으로 변경 -sj-

--- Bombom/src/main/java/com/kh/bom/point/model/service/PointService.java
+++ Bombom/src/main/java/com/kh/bom/point/model/service/PointService.java
@@ -1,17 +1,20 @@
 package com.kh.bom.point.model.service;
 
 import java.util.List;
-
+import java.util.Map;
 
 import com.kh.bom.point.model.vo.Point;
 
 public interface PointService {
 
 	
-	List<Point> selectPointList(String memNo, int cPage, int numPerpage);
+	List<Point> selectPointList(Map<String, String> map, int cPage, int numPerpage);
 
 	int insertStampPoint(Point p) throws Exception ;
 
-	int selectCount(String memNo);
+	int selectCount(Map<String, String> map);
+
+	int selectTotalPoint(String memNo);
+
 
 }
