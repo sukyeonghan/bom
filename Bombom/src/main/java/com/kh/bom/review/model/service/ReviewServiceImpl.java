9773236cde733219c9ee6d92f0dff32dfe438662
i9874 <i987412563i@gmail.com>
2021-01-08 20:23:43 +0900

    #구매평 작성 수정 중 -njs-

--- Bombom/src/main/java/com/kh/bom/review/model/service/ReviewServiceImpl.java
+++ Bombom/src/main/java/com/kh/bom/review/model/service/ReviewServiceImpl.java
@@ -38,21 +38,34 @@ public class ReviewServiceImpl implements ReviewService {
 	public int insertReview(Review r) throws Exception{
 		
 		int result = 0;
+		//INSERT INTO REVIEW VALUES('R'||SEQ_REV_NO.NEXTVAL,'P150','M23','20210108-837430',5,'리뷰작성칸/잠깐 이렇게 되면 PRODUCT_OPTION도 들어가야되나','이미지.JPG',SYSDATE,'N');
+		//리뷰작성 성공
+		/*
+		 * if(result>0) { //UPDATE INORDER SET REV_YN='Y' WHERE
+		 * ORDER_NO='20210108-837430' AND PDT_NO='P150' AND PDT_OPTION_NO='PO120'; }
+		 */
+		
 		try {
 			result =  dao.insertReview(session, r);
 
-			String msg = "";
-			int plus = 0;
-		
-			if(r.getRevImage()!=null) {
-				msg="포토구매평 작성";
-				plus=500;
-			}else {
-				msg="구매평 작성";
-				plus=200;
+			//구매평 작성 성공
+			if(result>0) {
+				//포인트 지급
+				String msg = "";
+				int plus = 0;
+			
+				if(r.getRevImage()!=null) {
+					msg="포토구매평 작성";
+					plus=500;
+				}else {
+					msg="구매평 작성";
+					plus=200;
+				}
+				Point p = new Point(r.getMemNo(),null,null,msg,plus);				
+				result = pointdao.insertStampPoint(session, p);
+				
+				//inorder rev_yn='Y'로 변경(구매평작성여부)
 			}
-			Point p = new Point(r.getMemNo(),null,null,msg,plus);				
-			result = pointdao.insertStampPoint(session, p);
 	
 		} catch (Exception e) {
 			e.printStackTrace();
